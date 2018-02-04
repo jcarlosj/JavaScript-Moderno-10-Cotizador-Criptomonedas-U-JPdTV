1b12d03709eb52b97e19b6041ad75cb3379090af
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-04 10:37:38 -0500

    Crea opciones del elemento SELECT 'Criptomonedas', datos obtenidos del REST API

--- js/interfaz.js
+++ js/interfaz.js
@@ -8,15 +8,28 @@ class Interfaz {
 
     /* Creamos un método init() al Estilo Angular para no sobre cargar el constructor */
     init() {
-        this .construirSelect();
+        this .construirSelectCriptomonedas();
     }
 
     /* Contruye un elemento select apartir de datos (Monedas) */
-    construirSelect() {
+    construirSelectCriptomonedas() {
         // Obtiene un 'Array' de monedas de la API 
         cotizador .obtenerMonedasAPI()
             .then( data => {
-                console .log( data );
+                console .log( data );                                                   // Objeto con un 'Array' monedas
+                // Crear un elemento SELECT
+                const monedas = data .monedas,                                          // 'Array' de Objetos con cada moneda
+                      select = document .getElementById( 'criptomoneda' );              // Obtiene el elemento select del DOM
+            
+                // Recorre y construye SELECT con los datos de la REST API
+                monedas .forEach( moneda => {
+                    // Agrega ID
+                    const option = document .createElement( 'option' );                 // Crea un elemento 'option'
+                    
+                    option .value = moneda .id;                                         // Agrega la propiedad 'value' al elemento 'option'
+                    option .appendChild( document .createTextNode( moneda .name ) );    // Agrega un nodo de texto al elemento 'option'
+                    select .appendChild( option );                                      // Agrega el elemento 'option' dentro del 'select'    
+                });
             });
     }
 
