e2a3e668ad9252fdd938e2db9f3dbb1bcf744ee1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-04 12:55:45 -0500

    Despliega un Spinner antes de realizar cotización (Simulando Búsqueda)

--- js/interfaz.js
+++ js/interfaz.js
@@ -56,6 +56,15 @@ class Interfaz {
               valor = cotizacion[ etiquetaMoneda ];                                     // Obtiene el valor de la moneda de acuerdo a la etiqueta                                           
         let templateHTML = '';                                                          // Inicializa un Template vacío
 
+        /* Eliminar Resultado de Cotización Previa */
+        const resultadoCotizacion = document .querySelector( '#resultado > div' );      // Selecciona el elemento donde se despliega el resultado de cada cotización
+        // Valida si existe cotización previa
+        if( resultadoCotizacion ) {
+            resultadoCotizacion .remove();                                              // Si exite resultado de cotización anterior lo elimina del DOM
+        }
+
+        this .mostrarSpinner();                                                         // Despliega el Spinner (Simulación de búsqueda)
+
         console .log( `Valor de 1 ${ cotizacion .name } en ${ idMoneda .toUpperCase() } = ${ valor }` ); 
         
         // Obtenemos la última actualización
@@ -68,15 +77,29 @@ class Interfaz {
                 <div class="card-content white-text">
                     <span class="card-title">El precio de ${ cotizacion .name } a moneda ${ idMoneda .toUpperCase() } es de: ${ valor } </span>
                     <hr />
-                    <p><b>Última hora:</b>${ cotizacion .percent_change_1h } </p>
-                    <p><b>Último día:</b>${ cotizacion .percent_change_24h } </p>
-                    <p><b>Últimos 7 días:</b>${ cotizacion .percent_change_7d } </p>
-                    <p><b>Última actualización:</b> ${ fecha .getHours() }:${ fecha .getMinutes() } horas</p>
+                    <p><b>Última hora: </b>${ cotizacion .percent_change_1h } </p>
+                    <p><b>Último día: </b>${ cotizacion .percent_change_24h } </p>
+                    <p><b>Últimos 7 días: </b>${ cotizacion .percent_change_7d } </p>
+                    <p><b>Última actualización: </b> ${ fecha .getHours() }:${ fecha .getMinutes() } horas</p>
                 </div>
             </div>
         `;
 
-        document .querySelector( '#resultado' ) .innerHTML = templateHTML;              // Agrega el Template al DOM   
+        // Oculta el Spinner y Muestra el resultado de la cotización
+        setTimeout( () => {
+            document .querySelector( '.spinner img' ) .remove();                            // Oculta el Spinner
+            document .querySelector( '#resultado' ) .innerHTML = templateHTML;              // Agrega el Template al DOM   
+        }, 3000 );      // 3 segundos
+        
     }
+
+    // Muestra un Spinner cuando se realiza la cotización (Simula búsqueda)
+    mostrarSpinner() {
+        const spinner = document .createElement( 'img' );                               // Crea un elemento 'img'
+
+        spinner .src = 'img/spinner.gif';                                               // Agrega la propiedad 'src' y su valor al elemento 'img'
+        document .querySelector( '.spinner' ) .appendChild( spinner );                  // Agrega el elemento 'img' al elemento con la clase 'spinner' en el DOM   
+    }
+
 
 }
